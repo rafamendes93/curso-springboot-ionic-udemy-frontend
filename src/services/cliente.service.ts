713600856feeb736defc48f17ff5e11eef6121ac
Rafael Mendes <rafaelmendes93@outlook.com>
2018-09-10 19:36:19 -0300

    Tela de confirmação de pedido

--- src/services/cliente.service.ts
+++ src/services/cliente.service.ts
@@ -13,6 +13,13 @@ export class ClienteService {
 
   }
 
+  findById(id: string){
+    return this.http.get(
+      `${API_CONFIG.baseUrl}/clientes/${id}`
+    );
+  }
+
+
   findByEmail(email:String){
     return this.http.get(
       `${API_CONFIG.baseUrl}/clientes/email?value=${email}`
