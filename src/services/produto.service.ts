e1b24546d274a2e071938dfc5c3f282f1f0459f1
Rafael Mendes <rafaelmendes93@outlook.com>
2018-09-10 11:34:15 -0300

    Criando pagina de detalhes do produto (mockado)

--- src/services/produto.service.ts
+++ src/services/produto.service.ts
@@ -1,6 +1,7 @@
 import {HttpClient} from "@angular/common/http";
 import {API_CONFIG} from "../config/api.config";
 import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class ProdutoService {
@@ -10,6 +11,11 @@ export class ProdutoService {
 
   findByCategoria(categoria_id : string){
     return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+  }
+
+  getSmallImageFromBucket(id: string): Observable<any>{
+    let url = `${API_CONFIG.bucketUrl}/prod${id}-small.jpg`;
+    return this.http.get(url,{responseType : 'blob'});
   }
 
 }
