dfe0094e24761048b27e7a316df44101b7cd3307
Rafael Mendes <rafaelmendes93@outlook.com>
2018-09-10 19:49:34 -0300

    Salvando o pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import {CartService} from "../../services/domain/cart.service";
 import {ClienteDTO} from "../../models/cliente.dto";
 import {EnderecoDTO} from "../../models/endereco.dto";
 import {ClienteService} from "../../services/cliente.service";
+import {PedidoService} from "../../services/domain/pedido.service";
 
 @IonicPage()
 @Component({
@@ -25,7 +26,8 @@ export class OrderConfirmationPage {
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public cartService : CartService,
-              public clienteService: ClienteService) {
+              public clienteService: ClienteService,
+              public pedidoService : PedidoService) {
 
     this.pedido = this.navParams.get('pedido');
   }
@@ -49,5 +51,21 @@ export class OrderConfirmationPage {
 
   total(){
     return this.cartService.total();
+  }
+
+  checkout(){
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+        this.cartService.createOrClearCart();
+        console.log(response.headers.get('location'));
+      },error1 => {
+        if(error1.status == 403){
+          this.navCtrl.setRoot('HomePage');
+        }
+      })
+  }
+
+  back(){
+    this.navCtrl.setRoot('CartPage');
   }
 }
